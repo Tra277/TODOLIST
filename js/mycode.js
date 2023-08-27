1ff7226080e18454c7b53fd577acf3faac39ecfc
Tra277 <sontra2773@gmail.com>
2023-08-27 21:10:16 +0700

    toDo App

---
+++ js/mycode.js
@@ -0,0 +1,47 @@
+const addBtn = document.getElementById('add-task-btn');
+const inputBox = document.querySelector('#input-box');
+const taskList = document.querySelector('.task-list');
+const audio = document.querySelector('audio');
+audio.play();
+
+addBtn.addEventListener('click',addToListTask);
+inputBox.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+        // Xử lý sự kiện khi người dùng nhấn phím Enter
+        addToListTask();
+    }
+});
+// Event with each task
+taskList.addEventListener('click',(e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        setData();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        setData();
+    }
+},false);
+loadData();
+
+// FUNCTION HANDLING
+function addToListTask(){
+    if(inputBox.value === ""){
+        alert('you must enter something...');
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;        
+        let span = document.createElement('span');
+        span.innerHTML = 'x';
+        li.appendChild(span);
+        taskList.appendChild(li);
+    }
+    inputBox.value = "";
+    setData();
+}
+function setData(){
+    localStorage.setItem('data',taskList.innerHTML);
+}
+
+function loadData(){
+    taskList.innerHTML = localStorage.getItem('data');
+}
